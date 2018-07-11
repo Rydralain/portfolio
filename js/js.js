30c0d7fa97260d9c741a736522fec7f91cb3b3c1
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-07-11 09:57:11 -0700

    Projects HTML & populator function

--- js/js.js
+++ js/js.js
@@ -9,4 +9,23 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('.scrollspy').scrollSpy();
   });
+
+  printProject("Title", "Cool description", "http://zombievision.net");
 });
+
+const projectList = [];
+
+function printProject(title, description, url){
+  let newProject = '<div class="col s12 m6">'+
+    '<div class="card teal">'+
+        '<div class="card-action teal lighten-2">'+
+            '<h5> <a class="black-text" href="'+url+'">'+title+'</a> </h5>'+
+        '</div>'+
+        '<div class="card-content">'+
+            description+
+        '</div>'+
+    '</div>'+
+  '</div>"'
+
+  $("#projects").append(newProject);
+}
