4618ae06b4e16ee25e022cef8ae9a6dd109b042c
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-07-11 08:58:37 -0700

    completed navbar and footer

--- js/js.js
+++ js/js.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
-    
   
+  // activate materialize sidenav
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
   });
+
+  // activate materialize scrollspy
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+});
