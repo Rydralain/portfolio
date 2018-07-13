a1465ac9bbdb043cd2f9c80cf725a7fbe1a3b88b
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-07-12 18:57:24 -0700

    completed project list maker

---
+++ js/projectList.js
@@ -0,0 +1,14 @@
+const projectList = {
+    recipeFinder: {
+        title: "Recipe Finder",
+        description: "App to search for and save your favorite recipes",
+        url: "http://bc.zombievision.net/p1/",
+        github: "https://github.com/JShafer92/Project1"
+    },
+    starWars: {
+      title: "Star Wars RPG",
+      description: "A simple RPG demonstrating constructors and HTML element creation in the DOM.",
+      url: "https://rydralain.github.io/star-wars-rpg/",
+      github: "https://github.com/Rydralain/star-wars-rpg"
+    }
+  };
